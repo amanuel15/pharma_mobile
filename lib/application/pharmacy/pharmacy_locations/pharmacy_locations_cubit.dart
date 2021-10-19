f1ca1f413fc8236301fc5028c71916da380813cd
XERZES <sibtesh@gmail.com>
2021-10-19 15:11:05 +0300

    test push 1

--- lib/application/pharmacy/pharmacy_locations/pharmacy_locations_cubit.dart
+++ lib/application/pharmacy/pharmacy_locations/pharmacy_locations_cubit.dart
@@ -39,7 +39,7 @@ class PharmacyLocationsCubit extends Cubit<PharmacyLocationsState> {
     );
     BitmapDescriptor mapMarker = await setCustomMarker();
     possiblePharmacies.when(
-      (error) => print("has failed to load pharmacies "),
+      (error) => print("has failed to load pharmacies... "),
       (success) {
         success.forEach(
           (pharmacy) {
